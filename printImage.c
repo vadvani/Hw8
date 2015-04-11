1a0db78eaa6fa99f75dc37c2d8a959896f73ef1c
vadvani <vadvani96@gmail.com>
2015-04-10 22:49:50 -0400

    printImage program

---
+++ printImage.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+struct image* imageCreate(void) {
+	struct image* i;
+	int c;
+
+	i = malloc(sizeof(struct image));
+	assert(i);
+
+	scanf ("P5 %d %d 255\n", &(i->width), &(i->height));
+	
+	i->image = malloc(sizeof(int*) * i->height); /*using right sizeof here???*/
+	assert(i->image);
+	for (int j = 0; j < i->width; j++) {
+		i->image[j] = malloc(sizeof(int) * i->width);
+		assert(i->image[j]);
+	}
+
+	for (int j=0; j < i->height; j++) {
+		for (int k=0; k < i->width; k++) {
+			c = getchar();
+			i->image[j][k] = c;
+		}
+	}
+
+	return i;
+}
+
+void printImage(struct image* i) {
+	printf("P5 %d %d 255\n", i->width, i->height);
+	for (int j=0; j < i->height; j++) {
+		for (int k=0; k < i->width; k++) {
+			printf("%d", i->image[j][k]);
+		}
+	}
+}
+
+int main (int argc, char** argv) {
+
+	struct image* i;
+	
+	i = imageCreate();
+	printImage(i);
+
+	return 0;
+}
