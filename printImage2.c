194c02c750b9514d335f6b215f52017ab2ebf6e6
vadvani <vadvani96@gmail.com>
2015-04-14 11:08:17 -0400

    print function to generate test images

---
+++ printImage2.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+struct image{
+	int width;
+	int height;
+	int** image;
+};
+
+struct image* imageCreate(void) {
+	struct image* i;
+	int c;
+
+	i = malloc(sizeof(struct image));
+	assert(i);
+
+	scanf ("P5 %d %d 255\n", &(i->width), &(i->height));
+	
+	i->image = malloc(sizeof(int*) * i->height); /*using right sizeof here???*/
+	assert(i->image);
+	for (int j = 0; j < i->width; j++) {
+		i->image[j] = malloc(sizeof(int) * i->width);
+		assert(i->image[j]);
+	}
+
+	for (int j=0; j < i->height; j++) {
+		for (int k=0; k < i->width; k++) {
+			c = getchar();
+			i->image[j][k] = c;
+		}
+	}
+
+	return i;
+}
+
+void printImage(struct image* i) {
+	printf("P5 %d %d 255\n", i->width, i->height);
+	for (int j=0; j < i->height; j++) {
+		for (int k=0; k < i->width; k++) {
+			printf("%d", i->image[j][k]);
+		}
+	}
+}
+
+int main (int argc, char** argv) {
+
+	struct image* i;
+	i->width = 2;
+	i->height = 3;
+	i->image = malloc(sizeof(*int) * 3);
+	for (int j = 0; j < height; j++) {
+		i->image[j] = malloc(sizeof(int) * 2);
+	}
+
+	i->image[0][0]=0;
+	i->image[0][1]=1;
+	i->image[1][0]=0;
+	i->image[1][1]=1;
+	i->image[2][0]=0;
+	i->image[2][1]=1;
+	
+	/*i = imageCreate();*/
+	printImage(i);
+
+	return 0;
+}
